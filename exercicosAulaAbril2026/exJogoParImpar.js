8f09d9ce18cdb2b0f9c269648f86db75ca5dd4c0
anabeatrizmanga <anabeatrizmanga21@gmail.com>
2026-04-27 11:12:30 -0300

    aula 2704

--- exercicosAulaAbril2026/exJogoParImpar.js
+++ exercicosAulaAbril2026/exJogoParImpar.js
@@ -1,11 +1,12 @@
-//criando referência para os elementos manipulados pela function
-var inNome = document.getElementById("inNome");
-var rbPar = document.getElementById("rbPar");
-var rbImpar = document.getElementById("rbImpar");
-var outNumero = document.getElementById("outNumero");
-var outResultado = document.getElementById("outResultado");
-var btJogar = document.getElementById("btJogar");
+const inNome = document.getElementById("inNome");
+const rbPar = document.getElementById("rbPar");
+const rbImpar = document.getElementById("rbImpar");
+const outNumero = document.getElementById("outNumero");
+const outResultado = document.getElementById("outResultado");
+const btJogar = document.getElementById("btJogar");
 
+
+btJogar.addEventListener("click", jogarParImpar);
 function jogarParImpar(){
     //obtendo o valor dos campos preenchidos do documento
     var nome = inNome.value;
@@ -13,7 +14,13 @@ function jogarParImpar(){
     var impar = rbImpar.checked; //true se o radio button Ímpar estiver marcado
     
     //verificando se nome foi preenchido e Par/Impar selecionado
-    if (nome != "" && (par != false || impar != false)){
+    if (nome == ""){
+        alert("Campo Nome do Jogador não foi preenchido!");
+        inNome.focus();
+    } else if (par == false && impar == false){
+        alert("Não foi selecionada a opçao Par ou Ímpar!");
+        rbPar.checked = true;
+    } else {
          //gerando um valor entre menorValor e maiorValor
         const menorValor = 1; //menor valor do range a ser gerado aleatoriamente
         const maiorValor = 100; //maior valor do range a ser gerado aleatoriamente
@@ -21,18 +28,15 @@ function jogarParImpar(){
     
         var resultado = (numAleatorio % 2 == 0) ? "Par" : "Ímpar";
         
-        outNumeroGerado.textContent = "Número Gerado => " + numAleatorio;
+        outNumeroGerado.innerHTML = "Número Gerado => " + numAleatorio;
 
         //verificando se o usuário acertou
         if ( (par && resultado == "Par") || (impar && resultado == "Ímpar") ) {
-            outResultadoJogo.textContent = "Parabéns " + nome + " você ganhou!";
+            outResultadoJogo.innerHTML = "Parabéns " + nome + " você ganhou!";
             outResultadoJogo.style.color = "green";
         } else {
-            outResultadoJogo.textContent = "Ops " + nome + " você perdeu!";
+            outResultadoJogo.innerHTML = "Ops " + nome + " você perdeu!";
             outResultadoJogo.style.color = "red";
         }
-    } else { //se não foi informado o nome ou um dos radio buttons não foi selecionado
-       alert ("Jogador, você precisa informar seu nome e selecionar Par ou Ímpar!");
     }
 }
-btJogar.addEventListener("click", jogarParImpar);
